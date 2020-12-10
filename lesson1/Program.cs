6790b4e4f9914d5b60a6069c97982529e52a8154
kingnsk <kingnsk2020@gmail.com>
2020-12-11 01:14:28 +0700

    added solution lesson1

---
+++ lesson1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как Вас зовут?");
+            // Читаем имя пользователя с консоли в переменную userName
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Привет, {userName}, сегодня {DateTime.Now}");
+            Console.ReadLine();
+        }
+    }
+}
