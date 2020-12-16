74d4e47effaefa5ec6132f59d240a281f9674336
kingnsk <kingnsk2020@gmail.com>
2020-12-17 01:23:56 +0700

    Выполнено задание 2 урока 3

---
+++ lesson3/lesson3_2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] phoneBook = new string[5, 2];
+
+            //Заполним телефонный справочник
+
+            for (int i = 0; i < phoneBook.GetLength(0); i++)
+            {
+                Console.WriteLine($"Введите данные контакта №{i}");
+                Console.WriteLine("Имя:");
+                phoneBook[i, 0] = Console.ReadLine();
+                Console.WriteLine("Телефон/e-mail:");
+                phoneBook[i, 1] = Console.ReadLine();
+
+            }
+
+            //Напечатаем справочник
+
+            for (int i = 0; i < phoneBook.GetLength(0); i++)
+            {
+                Console.WriteLine($"{i}      Имя: {phoneBook[i,0]}             Телефон/e-mail: {phoneBook[i,1]} ");
+            }
+        }
+    }
+}
