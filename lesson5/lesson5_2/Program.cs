e93ca02ead2df972fa1bb048388ba2701fb7b355
kingnsk <kingnsk2020@gmail.com>
2020-12-25 14:37:44 +0700

    Урок 5: Выполнены задания 1-5

---
+++ lesson5/lesson5_2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace lesson5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "startup.txt";
+            string output = DateTime.Now.ToString("T")+Environment.NewLine;
+            File.AppendAllText(filename, output);
+            Console.WriteLine("Текущее время записано в startup.txt");
+        }
+    }
+}
---
+++ lesson5/lesson5_5/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+//using System.Buffers;
+using System.Text.Json;
+using System.Collections;
+
+namespace lesson5_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "tasks.json";
+            int i=0;
+            string status,json;
+
+            List<ToDoClass> ToDoList = new List<ToDoClass>();
+            
+            if (File.Exists(filename))
+            {
+                Console.WriteLine("Файл task.json существует, читаем список задач...");
+                json = File.ReadAllText(filename);
+                ToDoList = JsonSerializer.Deserialize<List<ToDoClass>>(json);
+            } 
+            else
+            {
+                //Создаем базовый список задач
+                ToDoList.Add(new ToDoClass() { Title = "Задание 1", IsDone = false });
+                ToDoList.Add(new ToDoClass() { Title = "Задание 2", IsDone = false });
+                ToDoList.Add(new ToDoClass() { Title = "Задание 3", IsDone = false });
+                ToDoList.Add(new ToDoClass() { Title = "Задание 4", IsDone = true });
+                ToDoList.Add(new ToDoClass() { Title = "Задание 5", IsDone = false });
+            }
+
+            //Выводим список задач
+            foreach(ToDoClass toDo in ToDoList)
+            {
+                status=toDo.IsDone ? "[х]" : "[ ]";
+                Console.WriteLine($"{status} {i}: Задача: {toDo.Title} Статус: {toDo.IsDone}");
+                i++;
+            }
+
+            Console.WriteLine("Введите номер задачи:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            ToDoList[number].IsDone = true;
+
+            i = 0;
+            foreach (ToDoClass toDo in ToDoList)
+            {
+                status = toDo.IsDone ? "[х]" : "[ ]";
+                Console.WriteLine($"{status} {i}: Задача: {toDo.Title} Статус: {toDo.IsDone}");
+                i++;
+            }
+
+            json = JsonSerializer.Serialize(ToDoList);
+            File.WriteAllText(filename, json);
+
+        }
+    }
+}
