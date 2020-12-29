205d9556b3fad0c32a47cf3aa614bfaeee687b3a
kingnsk <kingnsk2020@gmail.com>
2020-12-30 01:44:30 +0700

    задания для урока №7

---
+++ lesson7/lesson7_2/lesson7_2/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("lesson7_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("lesson7_1")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("af6b713b-66e0-4f0b-8b50-d9a63d23182a")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
