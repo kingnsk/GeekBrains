67472fc3e7331c31957fa0aa9f63a3ebd2cdbda4
kingnsk <kingnsk2020@gmail.com>
2021-01-20 02:10:53 +0700

    Выполнены задания 1-2 Урока 2

---
+++ Block2/bl2_less2/bl2_less2_1/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bl2_less2_1
+{
+    class Program
+    {
+        public class Node
+        {
+            public int Value { get; set; }
+            public Node NextNode { get; set; }
+            public Node PrevNode { get; set; }
+        }
+
+        //Начальную и конечную ноду нужно хранить в самой реализации интерфейса
+        public interface ILinkedList
+        {
+            int GetCount(); // возвращает количество элементов в списке
+            void AddNode(int value);  // добавляет новый элемент списка
+            void AddNodeAfter(Node node, int value); // добавляет новый элемент списка после определённого элемента
+            void RemoveNode(int index); // удаляет элемент по порядковому номеру
+            void RemoveNode(Node node); // удаляет указанный элемент
+            Node FindNode(int searchValue); // ищет элемент по его значению
+        }
+
+        public class MyLinkedList : ILinkedList
+        {
+            private int _count = 0;
+            private Node _startNode;
+            private Node _endNode;
+            private Node _currentNode;
+            private Node _newNode;
+
+            public void AddNode(int value)
+            {
+                if (_startNode == null)
+                {
+                    _startNode = new Node { Value = value };
+                    _endNode = _startNode;
+                } 
+                
+                else if (_startNode == _endNode)
+                {
+                    _newNode = new Node { Value = value };
+                    _endNode = _newNode;
+                    _startNode.NextNode = _newNode;
+                    _endNode.PrevNode = _startNode;
+                }
+
+                else
+                {
+                    _newNode = new Node { Value = value, PrevNode=_endNode };
+                    _endNode.NextNode = _newNode;
+                    _endNode = _newNode;
+                }
+                _count++;
+            }
+
+            public void AddNodeAfter(Node node, int value)
+            {
+                _newNode = new Node { Value = value, NextNode = _currentNode, PrevNode = node };
+                _currentNode = node.NextNode;
+                node.NextNode = _newNode;
+                _currentNode.PrevNode = _newNode;
+                _count++;
+            }
+
+            public Node FindNode(int searchValue)
+            {
+                _currentNode = _startNode;
+                while (_currentNode!=null)
+                {
+                    if (_currentNode.Value == searchValue)
+                        return _currentNode;
+                    _currentNode = _currentNode.NextNode;
+                }
+                return null;
+            }
+
+            public int GetCount()
+            {
+                return _count;
+            }
+
+            public void RemoveNode(int index)
+            {
+                _currentNode = _startNode;
+                for (int i = 0; i < index; i++)
+                {
+                    _currentNode = _currentNode.NextNode;
+                }
+                RemoveNode(_currentNode);
+            }
+
+            public void RemoveNode(Node node)
+            {
+                if (node.PrevNode != null)
+                {
+                    node.PrevNode.NextNode = node.NextNode;
+                }
+
+                if (node.NextNode != null)
+                {
+                    node.NextNode.PrevNode = node.PrevNode;
+                }
+
+                _count--;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //Создадим и заполним двусвязный список 
+            var linkedList = new MyLinkedList();
+            linkedList.AddNode(18);
+            linkedList.AddNode(22);
+            linkedList.AddNode(77);
+            linkedList.AddNode(88);
+            Console.WriteLine($"{linkedList.GetCount()}");
+            
+            // Добавим новую ноду со значением 11 после ноды со значением 77
+            Node temp = linkedList.FindNode(77);
+            if (temp!=null)
+            {
+                linkedList.AddNodeAfter(temp, 11);
+            }
+            Console.WriteLine($"{linkedList.GetCount()}");
+
+            //удалинм ноду с индексом 0
+            linkedList.RemoveNode(0);
+            Console.WriteLine($"{linkedList.GetCount()}");
+
+            //Удалим ноду со значением 22
+            temp = linkedList.FindNode(22);
+            if (temp != null)
+            {
+                linkedList.RemoveNode(temp);
+            }
+            Console.WriteLine($"{linkedList.GetCount()}");
+            
+        }
+    }
+}
---
+++ Block2/bl2_less2/bl2_less2_2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bl2_less2_2
+{
+    class Program
+    {
+        public static int BinarySearch(int[] inputArray, int searchValue)
+        {
+            int min = 0;
+            int max = inputArray.Length - 1;
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+                if (searchValue == inputArray[mid])
+                {
+                    return mid;
+                }
+                else if (searchValue < inputArray[mid])
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+            }
+            return -1;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            int [] testArray = { 33, 22, 11, 98, 5, 7, 32, 77, 99, 1, 0, 19, 28, 33 };
+            Array.Sort(testArray);
+            
+            Console.WriteLine("Введите число для поиска в массиве:");
+            int search = Convert.ToInt32(Console.ReadLine());
+            int index = BinarySearch(testArray, search);
+
+            Console.WriteLine("Ищем в массиве:");
+            for (int i = 0; i < testArray.Length; i++)
+            {
+                Console.WriteLine($"{testArray[i]}");
+            }
+
+            if (index != -1)
+            {
+                Console.WriteLine($"Искомый элемент находится в позиции {index}");
+            } else
+            {
+                Console.WriteLine("Искомый элемент не найден.");
+            }
+            // Алгоритм имеет логарифмическую сложность O(log n) т.к. на каждой итерации входные данные уменьшаются в 2 раза
+        }
+    }
+}
