ca6e986ab8e71922314321b522389d48de43b9cb
kingnsk <kingnsk2020@gmail.com>
2021-01-29 23:23:27 +0700

    Выполнено задание 1 урока 4 (Блок2) Выполнено задание 2 урока 4 (Блок2)

---
+++ Block2/bl2_less4/bl2_less4_2/Node.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+
+namespace bl2_less4_2
+{
+    class Node<T> : IComparable
+        where T: IComparable
+    {
+        public T Data { get; set; }
+        public Node<T> Left { get; set; }
+        public Node<T> Right { get; set; }
+        public Node<T> Parent { get; set; }
+
+        public Node(T data)
+        {
+            Data = data;
+        }
+
+        public Node(T data, Node<T> parent)
+        {
+            Data = data;
+            Parent = parent;
+        }
+
+        public Node(T data, Node<T> left, Node<T> right, Node<T> parent)
+        {
+            Data = data;
+            Left = left;
+            Right = right;
+            Parent = parent;
+        }
+
+        public void Add(T data)
+        {
+            var node = new Node<T>(data);
+            if (node.Data.CompareTo(Data) == -1)
+            {
+                if(Left == null)
+                {
+                    Left = new Node<T>(data, this);
+                }
+                else
+                {
+                    Left.Add(data);
+                }
+            }
+            else
+            {
+                if(Right==null)
+                {
+                    Right = new Node<T>(data, this);
+                }
+                else
+                {
+                    Right.Add(data);
+                }
+            }
+        }
+
+        public int CompareTo(object obj)
+        {
+            if(obj is Node<T> item)
+            {
+                return Data.CompareTo(item);
+            }
+            else
+            {
+                throw new ArgumentException("Типы не совпадают!");
+            }
+        }
+
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+    }
+}
---
+++ Block2/bl2_less4/bl2_less4_2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bl2_less4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int searchData = 88;
+            var tree = new Tree<int>();
+            tree.Add(50);
+            tree.Add(33);
+            tree.Add(71);
+            tree.Add(12);
+            tree.Add(28);
+            tree.Add(88);
+            tree.Add(67);
+            tree.Add(90);
+            tree.Add(44);
+
+            Console.WriteLine("Бинарное дерево поиска (BST):");
+            tree.PrintToConsoleOriginal();
+
+            Console.WriteLine("Prefix обход дерева:");
+            foreach (var item in tree.preOrder())
+            {
+                Console.Write(item+", ");
+            }
+
+            Console.WriteLine("\nPostfix обход дерева:");
+            foreach (var item in tree.postOrder())
+            {
+                Console.Write(item + ", ");
+            }
+
+            Console.WriteLine("\nInfix обход дерева:");
+            foreach (var item in tree.inOrder())
+            {
+                Console.Write(item + ", ");
+            }
+
+            Console.WriteLine($"\n\nИщем в дереве узел со значением {searchData}");
+
+            var tmp = tree.search(tree.Root, searchData);
+            if (tmp != null)
+            {
+                Console.WriteLine($"Узел:{tmp.Data} Левый потомок: {tmp.Left} Правый потомок: {tmp.Right} Родитель: {tmp.Parent}");
+            } else
+            {
+                Console.WriteLine($"Узел:{searchData} Не найден!");
+            }
+
+            Console.WriteLine($"\nУдаляем узел:{searchData}");
+            Console.WriteLine($"{tree.remove(tree.Root, searchData)}");
+            Console.WriteLine("Получилось дерево:");
+            tree.PrintToConsoleOriginal();
+            
+        }
+    }
+}
