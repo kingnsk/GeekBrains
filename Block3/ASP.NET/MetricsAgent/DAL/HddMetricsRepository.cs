92464332cb1b417167e568b376f67f1ba1fcc863
kingnsk <kingnsk2020@gmail.com>
2021-04-04 14:07:52 +0700

    Реализован репозиторий для Агента HddMetric v2

---
+++ Block3/ASP.NET/MetricsAgent/DAL/HddMetricsRepository.cs
@@ -0,0 +1,120 @@
+﻿using MetricsAgent.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+
+namespace MetricsAgent.DAL
+{
+    // маркировочный интерфейс
+    // необходим, чтобы проверить работу репозитория на тесте-заглушке
+    public interface IHddMetricsRepository : IRepository<HddMetric>
+    {
+
+    }
+
+    public class HddMetricsRepository : IHddMetricsRepository
+    {
+        // наше соединение с базой данных
+        private SQLiteConnection connection;
+
+        // инжектируем соединение с базой данных в наш репозиторий через конструктор
+        public HddMetricsRepository(SQLiteConnection connection)
+        {
+            this.connection = connection;
+        }
+
+        public void Create(HddMetric item)
+        {
+            // создаем команду
+            using var cmd = new SQLiteCommand(connection);
+            // прописываем в команду SQL запрос на вставку данных
+            cmd.CommandText = "INSERT INTO hddmetrics(value, time) VALUES(@value, @time)";
+
+            // добавляем параметры в запрос из нашего объекта
+            cmd.Parameters.AddWithValue("@value", item.Value);
+
+            cmd.Parameters.AddWithValue("@time", item.Time);
+            // подготовка команды к выполнению
+            cmd.Prepare();
+
+            // выполнение команды
+            cmd.ExecuteNonQuery();
+        }
+
+        public void Delete(int id)
+        {
+            using var cmd = new SQLiteCommand(connection);
+            // прописываем в команду SQL запрос на удаление данных
+            cmd.CommandText = "DELETE FROM hddmetrics WHERE id=@id";
+
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.Prepare();
+            cmd.ExecuteNonQuery();
+        }
+
+        public void Update(HddMetric item)
+        {
+            using var cmd = new SQLiteCommand(connection);
+            // прописываем в команду SQL запрос на обновление данных
+            cmd.CommandText = "UPDATE hddmetrics SET value = @value, time = @time WHERE id=@id;";
+            cmd.Parameters.AddWithValue("@id", item.Id);
+            cmd.Parameters.AddWithValue("@value", item.Value);
+            cmd.Parameters.AddWithValue("@time", item.Time);
+            cmd.Prepare();
+
+            cmd.ExecuteNonQuery();
+        }
+
+        public IList<HddMetric> GetAll()
+        {
+            using var cmd = new SQLiteCommand(connection);
+
+            // прописываем в команду SQL запрос на получение всех данных из таблицы
+            cmd.CommandText = "SELECT * FROM hddmetrics";
+
+            var returnList = new List<HddMetric>();
+
+            using (SQLiteDataReader reader = cmd.ExecuteReader())
+            {
+                // пока есть что читать -- читаем
+                while (reader.Read())
+                {
+                    // добавляем объект в список возврата
+                    returnList.Add(new HddMetric
+                    {
+                        Id = reader.GetInt32(0),
+                        Value = reader.GetInt32(1),
+                        Time = reader.GetInt32(2)
+                    });
+                }
+            }
+
+            return returnList;
+        }
+
+        public HddMetric GetById(int id)
+        {
+            using var cmd = new SQLiteCommand(connection);
+            cmd.CommandText = "SELECT * FROM hddmetrics WHERE id=@id";
+            using (SQLiteDataReader reader = cmd.ExecuteReader())
+            {
+                // если удалось что то прочитать
+                if (reader.Read())
+                {
+                    // возвращаем прочитанное
+                    return new HddMetric
+                    {
+                        Id = reader.GetInt32(0),
+                        Value = reader.GetInt32(1),
+                        Time = reader.GetInt32(2)
+                    };
+                }
+                else
+                {
+                    // не нашлось запись по идентификатору, не делаем ничего
+                    return null;
+                }
+            }
+        }
+    }
+}
