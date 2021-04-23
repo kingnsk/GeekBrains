b11ecaa4a0e4575784b85ef9983087010af41ab6
kingnsk <kingnsk2020@gmail.com>
2021-04-24 01:52:24 +0700

    Первая попытка запустить базовый сервис WPF по методичке

--- Block3/ASP.NET/MetricsClient/MainWindow.xaml.cs
+++ Block3/ASP.NET/MetricsClient/MainWindow.xaml.cs
@@ -1,28 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace MetricsClient
+﻿using System.Windows;
+namespace MetricsManagerClient
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            CpuChart.ColumnServiesValues[0].Values.Add(48d);
         }
     }
 }
