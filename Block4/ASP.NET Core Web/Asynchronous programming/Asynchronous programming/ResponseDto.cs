e2abcf3b24b388154865ad66c4e9e48af56c14cf
kingnsk <kingnsk2020@gmail.com>
2021-05-04 23:53:11 +0700

    Выполнено первое задание по теме асинхронное программирование

---
+++ Block4/ASP.NET Core Web/Asynchronous programming/Asynchronous programming/ResponseDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asynchronous_programming
+{
+    class ResponseDto
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string body { get; set; }
+    }
+}
