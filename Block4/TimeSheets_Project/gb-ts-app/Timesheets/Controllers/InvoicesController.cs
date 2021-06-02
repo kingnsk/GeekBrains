097902f63383b70596762c5870444185d6fbe331
kingnsk <kingnsk2020@gmail.com>
2021-06-03 01:06:34 +0700

    Реализовано выписывание счетов

--- Block4/TimeSheets_Project/gb-ts-app/Timesheets/Controllers/InvoicesController.cs
+++ Block4/TimeSheets_Project/gb-ts-app/Timesheets/Controllers/InvoicesController.cs
@@ -4,23 +4,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Timesheets.Models.Dto;
+using Timesheets.Domain.Interfaces;
 
 namespace Timesheets.Controllers
 {
 
     public class InvoicesController: TimesheetBaseController
     {
+        private readonly IContractManager _contractManager;
+        private readonly IInvoiceManager _invoiceManager;
+
+        public InvoicesController(IContractManager contractManager, IInvoiceManager invoiceManager)
+        {
+            _contractManager = contractManager;
+            _invoiceManager = invoiceManager;
+        }
+
         /// <summary> Создает клиентский счет (invoice) </summary>
         [HttpPost]
         public async Task<IActionResult> Create([FromBody] InvoiceRequest invoiceRequest)
         {
-            var isAllowedToCreate = await _contractManager.CheckContractIsActive(sheet.ContractId);
+            var isAllowedToCreate = await _contractManager.CheckContractIsActive(invoiceRequest.ContractId);
 
             if (isAllowedToCreate != null && !(bool)isAllowedToCreate)
             {
-                return BadRequest($"Contract {sheet.ContractId} is not active or not found.");
+                return BadRequest($"Contract {invoiceRequest.ContractId} is not active or not found.");
             }
-            
+
+            var id = await _invoiceManager.Create(invoiceRequest);
+            return Ok(id);
         }
 
     }
--- Block4/TimeSheets_Project/gb-ts-app/Timesheets/Data/Interfaces/ISheetRepo.cs
+++ Block4/TimeSheets_Project/gb-ts-app/Timesheets/Data/Interfaces/ISheetRepo.cs
@@ -1,9 +1,12 @@
 ﻿using Timesheets.Models;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System;
 
 namespace Timesheets.Data.Interfaces
 {
     public interface ISheetRepo: IRepoBase<Sheet>
     {
-        
+        Task<IEnumerable<Sheet>> GetItemsForInvoice(Guid contractId, DateTime dateStart, DateTime dateEnd);
     }
 }
